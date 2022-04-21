749f808916d9b20e554b37d26b0ac685cbf21b66
chemoontheshy <xzfandzgx@gmail.com>
2022-04-21 23:37:40 +0800

    完善

--- src/showVideo/main.cpp
+++ src/showVideo/main.cpp
@@ -1,7 +1,30 @@
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <utils/vsnc_utils.h>
 
 int main()
 {
-
+	// 1.读取视频
+	cv::VideoCapture capture;
+	capture.open("..\\..\\3rdparty\\video\\1280x720_30fps_60s.mp4");
+	// 2.循环显示每一帧
+	throw_if(!capture.isOpened());
+	cv::Mat frame;
+	size_t num = 0;
+	while (true)
+	{
+		capture >> frame;
+		if (frame.empty()) break;
+		cv::imshow("video", frame);
+		auto saveFile = "../../3rdparty/video/save/" + std::to_string(vsnc::utils::__utc()) + ".png";
+		if (num % 60 == 0)
+		{
+			cv::imwrite(saveFile, frame);
+		}
+		num++;
+		cv::waitKey((1000 / 30));
+	
+	}
+	capture.release();
+	return 0;
 }
